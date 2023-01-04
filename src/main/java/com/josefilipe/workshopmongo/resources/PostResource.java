c0b5323ede5fc73bffc1b12468c1b920d68f9a1d
Filipe <filipeandrade000@gmail.com>
2023-01-04 18:31:16 -0300

    Obtendo um post por id

---
+++ src/main/java/com/josefilipe/workshopmongo/resources/PostResource.java
@@ -0,0 +1,40 @@
+package com.josefilipe.workshopmongo.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.josefilipe.workshopmongo.domain.Post;
+import com.josefilipe.workshopmongo.domain.User;
+import com.josefilipe.workshopmongo.dto.UserDTO;
+import com.josefilipe.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+}
---
+++ src/main/java/com/josefilipe/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.josefilipe.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josefilipe.workshopmongo.domain.Post;
+import com.josefilipe.workshopmongo.domain.User;
+import com.josefilipe.workshopmongo.dto.UserDTO;
+import com.josefilipe.workshopmongo.repository.PostRepository;
+import com.josefilipe.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+
+}
