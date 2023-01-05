721742ce981e3f859072d2dec3ed7f14e8f4ec06
Filipe <filipeandrade000@gmail.com>
2023-01-04 21:38:57 -0300

    Consulta com varios testes

--- src/main/java/com/josefilipe/workshopmongo/resources/PostResource.java
+++ src/main/java/com/josefilipe/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.josefilipe.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,4 +36,15 @@ public class PostResource {
 	}
 
 
+	@GetMapping("/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/josefilipe/workshopmongo/services/PostService.java
+++ src/main/java/com/josefilipe/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.josefilipe.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,19 +16,18 @@ public class PostService {
 
 	@Autowired
 	private PostRepository repo;
-
-	public List<Post> findAll() {
-		return repo.findAll();
-	}
-
-	public List<Post> findByTitle(String text) {
-		return repo.searchTitle(text);
-	}
 	
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-
 	}
-
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
